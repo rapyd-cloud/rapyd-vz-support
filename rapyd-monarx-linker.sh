eb53039f0bf1e47478393c3b3a7cfe467418e687
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-11-13 21:56:09 +1000

    Create rapyd-monarx-linker.sh
    linker for monarx to bind so file to php

---
+++ rapyd-monarx-linker.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+cd ~ 
+
+# define logic variables to determine which monarx linked so file we should deploy
+
+MONARX_PHP=$(php-config --version | cut -c -3)
+MONARX_PHPSTRIPPED=${MONARX_PHP/.}
+MONARX_INI_DIR=$(php-config --ini-dir)
+MONARX_EXT_DIR=$(php-config --extension-dir)
+MONARX_ARCH=$(arch | sed -e 's/arm64/aarch64/;s/amd64\|x86_64/x86-64/')
+
+MONARX_SOPATH="/usr/lib64/monarx-protect"
+MONARX_SO="monarxprotect-php$MONARX_PHPSTRIPPED.so"
+MONARX_INI_FILEPATH="$MONARX_INI_DIR/monarxprotect.ini"
+MONARX_LINKED="$MONARX_EXT_DIR/$MONARX_SO"
+
+# clean up and existing linked file and then link so
+if [ -L "$MONARX_LINKED" ]; then
+  rm -f "$MONARX_LINKED"
+fi
+ln -sf "$MONARX_SOPATH/$MONARX_SO" "$MONARX_EXT_DIR"
+
+# clean up and existing php.ini file and then create new one 
+
+if [ -f "$MONARX_INI_FILEPATH" ]; then
+  rm -f "$MONARX_INI_FILEPATH"
+fi
+echo "extension=$MONARX_SO" > "$MONARX_INI_FILEPATH"
+
+# end of monarx linker logic
