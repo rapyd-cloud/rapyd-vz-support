6afe08595543d3ea7a7eb75716810113ca5022c4
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-11-14 21:05:12 +1000

    Update rapyd-ocp-cleaner.sh
    add wp plugin logic

--- rapyd-ocp-cleaner.sh
+++ rapyd-ocp-cleaner.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
 cd /var/www/webroot/ROOT/
-wp plugin deactivate object-cache-pro || true
-wp plugin delete object-cache-pro || true
+
+wp plugin is-installed object-cache-pro
+
+if [ "$?" -eq 0 ]
+then
+   wp plugin is-active object-cache-pro
+ 
+   if [ "$?" -eq 0 ]
+     then
+       wp plugin deactivate object-cache-pro || true
+       wp plugin delete object-cache-pro || true
+    fi
+fi
+   
