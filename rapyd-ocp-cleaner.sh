8edb1b4a3f9d13d1936cbf432633bc2181d448c5
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-11-24 18:23:39 +1000

    Update rapyd-ocp-cleaner.sh
    force all wp cli commands to ignore error output using  --quiet

--- rapyd-ocp-cleaner.sh
+++ rapyd-ocp-cleaner.sh
@@ -4,16 +4,16 @@ cd /var/www/webroot/ROOT/
 
 # if installed clean up gracefully 
 
-wp plugin is-installed object-cache-pro
+wp plugin is-installed object-cache-pro --quiet
 
 if [ "$?" -eq 0 ]
 then
-   wp plugin is-active object-cache-pro
+   wp plugin is-active object-cache-pro --quiet
  
    if [ "$?" -eq 0 ]
      then
-       wp plugin deactivate object-cache-pro || true
-       wp plugin delete object-cache-pro || true
+       wp plugin deactivate object-cache-pro --quiet || true
+       wp plugin delete object-cache-pro --quiet || true
     fi
 fi
    
