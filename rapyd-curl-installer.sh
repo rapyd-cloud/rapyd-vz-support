3f216271dd8b88cecb824f5396a2162afee95704
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-12-03 00:16:19 +1000

    Update rapyd-curl-installer.sh
    enable http2 brotli libssh zlib

--- rapyd-curl-installer.sh
+++ rapyd-curl-installer.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 # Note - in future - we may want to update this with a more recent version 
+# refer notes from centos wiki
+# https://wiki.centos-webpanel.com/update-curl-to-latest-version-in-centos
 # The latest version number can always been found at https://curl.se/download/
 # This version should be built and packaged for Centos7
 # We will need to make a newer build for AlamaLinux in the near future 
@@ -8,9 +10,9 @@
 # start of script
 
 # 7.80.0 seems to be minimum viable version that doesnt lead to conflicts with php and openssl 
-# 8.4.0 is the current release - it contains a serious vulnerability patch 
-# currently we have no evidence that is has any observed issues with centos 7
 # VERSION=7.80.0
+# 7.88.1 is last version documented in centos wiki
+# VERSION=7.88.0
 # VERSION=7.88.1
 VERSION=8.4.0
 
@@ -23,27 +25,42 @@ else
 
   # this is a raw make clear && make && make install = very inefficient  
   # todo: convert to an RPM package 
+
   cd ~
 
   # dont update platform at this point - it seems to cause issues with php8.2.5 being partially updated to php8.2.12
   #sudo yum update -y
+
+  # check for compiler dependencies
   sudo yum install wget gcc openssl-devel make -y
+
+  # check for curl  new library dependencies
+  sudo yum install libssh libssh-devel libnghttp2-devel libnghttp2 libgsasl libgsasl-devel zstd libzstd-devel libzstd brotli brotli-devel libbrotli -y
   
   wget https://curl.haxx.se/download/curl-${VERSION}.tar.gz
   tar -xzvf curl-${VERSION}.tar.gz 
   sudo rm -f curl-${VERSION}.tar.gz
   cd curl-${VERSION}
-  ./configure --prefix=/usr/local --with-ssl
+
+  # setup build configuration
+  ./configure --prefix=/usr/local --with-ssl --with-zlib --with-gssapi --with-libssh --with-nghttp2 --with-openssl     
+  # ./configure --with-ssl --with-zlib --with-gssapi --with-libssh --with-nghttp2     --enable-ldap --enable-ldaps   --with-ngtcp2  --with-quiche
+
+  # run build process 
   sudo make clean
   sudo make
   sudo make install
   
+  # link to centos lib location
   sudo ln -sf /usr/local/lib/libcurl.so.4 /usr/lib/libcurl.so.4
   
+  # update linux library configuration 
   sudo ldconfig
   
+  # clean up
   cd ~
   rm -rf curl-${VERSION}
+
 fi
 
 # end of script
