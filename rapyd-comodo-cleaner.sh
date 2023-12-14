a86578965c11b3846bce0e9c86a1ef7b81c331e1
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-12-15 00:11:20 +1000

    Create rapyd-comodo-cleaner.sh
    comodo cleanup script

---
+++ rapyd-comodo-cleaner.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# must be run as litespeed user
+
+######################################################################################################################
+# renove Rapyd Comodo exclude rules 
+
+cd /var/www/conf/comodo_litespeed
+
+# remove preset excludes list
+rm -f 00_Rapyd_Excludes.conf
+
+# dont remove customer excludes list
+# rm -f 00_Rapyd_Customer_Excludes.conf
+
+# remove rapyd rules from rules.conf
+if [ -f "rules.conf" ]; then
+  sed -i '/00_Rapyd_Excludes/d' rules.conf
+  sed -i '/00_Rapyd_Customer_Excludes/d' rules.conf 
+fi
+
+######################################################################################################################
