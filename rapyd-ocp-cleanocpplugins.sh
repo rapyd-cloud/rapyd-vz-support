c2a69e7c9fa422563f740812fd5f10f701f8996e
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-12-22 12:51:23 +1000

    Update rapyd-ocp-cleanocpplugins.sh
    force--quiet 2>/dev/null option

--- rapyd-ocp-cleanocpplugins.sh
+++ rapyd-ocp-cleanocpplugins.sh
@@ -5,46 +5,46 @@ WP_ROOT="/var/www/webroot/ROOT"
 cd "$WP_ROOT"
 
 #litespeed cache redis object extension
-wp plugin is-installed litespeed-cache --quiet
+wp plugin is-installed litespeed-cache --quiet 2>/dev/null
 
 if [ "$?" -eq 0 ]
 then
-   wp plugin is-active litespeed-cache --quiet
+   wp plugin is-active litespeed-cache --quiet 2>/dev/null
 
    if [ "$?" -eq 0 ]
      then
 
        ## disable the ls object cache before doing any other actions
-       wp litespeed-option set object false  --quiet   
+       wp litespeed-option set object false --quiet 2>/dev/null
      
     fi
 fi
 
 # free version of redis object cache
-wp plugin is-installed redis-cache  --quiet
+wp plugin is-installed redis-cache --quiet 2>/dev/null
 
 if [ "$?" -eq 0 ]
 then
-   wp plugin is-active redis-cache  --quiet
+   wp plugin is-active redis-cache --quiet 2>/dev/null
  
    if [ "$?" -eq 0 ]
      then
-       wp plugin deactivate redis-cache  --quiet || true
-       wp plugin delete redis-cache  --quiet || true
+       wp plugin deactivate redis-cache --quiet 2>/dev/null || true
+       wp plugin delete redis-cache --quiet 2>/dev/null || true
     fi
 fi
 
 # commercial version 
-wp plugin is-installed object-cache-pro --quiet
+wp plugin is-installed object-cache-pro --quiet 2>/dev/null
 
 if [ "$?" -eq 0 ]
 then
-   wp plugin is-active object-cache-pro --quiet
+   wp plugin is-active object-cache-pro --quiet 2>/dev/null
  
    if [ "$?" -eq 0 ]
      then
-       wp plugin deactivate object-cache-pro --quiet || true
-       wp plugin delete object-cache-pro --quiet || true
+       wp plugin deactivate object-cache-pro --quiet 2>/dev/null || true
+       wp plugin delete object-cache-pro --quiet 2>/dev/null || true
     fi
 fi
 
@@ -56,4 +56,4 @@ rm -f "$WP_ROOT/wp-content/object-cache.php"
 rm -f "$WP_ROOT/wp-content/mu-plugins/redis-cache-pro.php"
 rm -rf "$WP_ROOT/wp-content/mu-plugins/redis-cache-pro"
 
-wp cache flush --quiet
+wp cache flush --quiet 2>/dev/null
