dcc9157342dade2ea2690388af446de6084a6909
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2023-12-30 14:29:36 +1000

    Create rapyd-ffmpeg-installer.sh
    Create ffmpeg installed with version checking for alamlinux and centos

---
+++ rapyd-ffmpeg-installer.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# must be run as root - su user
+
+######################################################################################################################
+# ffmpeg 
+
+if grep -a 'AlmaLinux' /etc/system-release ; then
+  # work out what we need to do here for AlmaLinux 
+  cd ~
+  echo "Installing ffmpeg for AlamLinux"
+
+  sudo dnf install epel-release -y
+  sudo dnf config-manager --set-enabled crb -y
+
+  sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm -y
+  sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
+
+  sudo dnf install ffmpeg ffmpeg-devel -y
+  
+else
+  cd ~
+  echo "Installing ffmpeg for Centos"
+
+  yum install epel-release -y
+  yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm -y
+  yum install ffmpeg ffmpeg-devel -y
+
+fi
+
+
