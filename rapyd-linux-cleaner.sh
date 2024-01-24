7ee63e319432d406620def61c50f7fcdd6b1bebb
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2024-01-24 16:47:12 +1000

    Update rapyd-linux-cleaner.sh

--- rapyd-linux-cleaner.sh
+++ rapyd-linux-cleaner.sh
@@ -5,15 +5,25 @@
 ######################################################################################################################
 # remove linux config 
 
+function removerapydbashrc  {
+  # for now just remove the .rapyd.bashrc file 
+  filepath="/usr/local/rapyd/rapyd-linux-files/.rapyd.bashrc
+  if [ -f "$filepath" ]; then
+    rm -rf $filepath
+  fi
+}
+
 if grep -a 'AlmaLinux' /etc/system-release ; then
   # work out what we need to do here for AlmaLinux 
   cd ~
   echo "Removing Linux Config for AlamLinux"
+  removerapydbashrc
   
 else
   cd ~
   echo "Removing linux Config for Centos"
-
+  removerapydbashrc
+  
 fi
 
 # end remove linux config 
