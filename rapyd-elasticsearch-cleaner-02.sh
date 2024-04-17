42fc83e895000f67c020751d9f889c3d94496617
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2024-04-17 22:24:14 +1000

    Update rapyd-elasticsearch-cleaner-02.sh

--- rapyd-elasticsearch-cleaner-02.sh
+++ rapyd-elasticsearch-cleaner-02.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-# must be run as litespeed user
+WP_ROOT="/var/www/webroot/ROOT"
 
-######################################################################################################################
-# remove  elasticpress  config 
+##################################################################################
+cd "$WP_ROOT"
 
-if grep -a 'AlmaLinux' /etc/system-release ; then
-  # work out what we need to do here for AlmaLinux 
-  cd ~
-  
-else
-  cd ~
-  
+# get the current list of all active plugins 
+# we are going to use this later to skip all installed plugins apart for those we want to test
+SKIPPLUGINS='^litespeed-cache$\|^object-cache-pro$\|^redis-cache$'
+SKIPLIST=$(wp plugin list --status=active --field=name --quiet --skip-plugins 2>/dev/null | grep -v $SKIPPLUGINS | tr '\n' ',' )
+
+##################################################################################
+# if installed clean up gracefully 
+wp --skip-plugins --skip-themes --quiet  plugin is-installed elasticpress   2>/dev/null
+
+if [ "$?" -eq 0 ]
+then
+   wp --skip-plugins --skip-themes --quiet  plugin is-active elasticpress  2>/dev/null
+ 
+   if [ "$?" -eq 0 ]
+     then
+       wp --skip-plugins --skip-themes --quiet plugin deactivate elasticpress   2>/dev/null || true
+       wp --skip-plugins --skip-themes --quiet plugin uninstall elasticpress  2>/dev/null || true
+    fi
 fi
 
-# end remove elasticpress config 
-######################################################################################################################
+# force a clear exit regardless of any errors in script results 
+
+exit 0
+##################################################################################
 
