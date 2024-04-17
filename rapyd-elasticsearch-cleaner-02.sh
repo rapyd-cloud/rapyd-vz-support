314c0880b2e570d0fc7c2d0e53a5abe808eae22f
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2024-04-17 21:28:21 +1000

    Update rapyd-elasticsearch-cleaner-02.sh

--- rapyd-elasticsearch-cleaner-02.sh
+++ rapyd-elasticsearch-cleaner-02.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# must be run as litespeed user
+
+######################################################################################################################
+# remove  elasticpress  config 
+
+if grep -a 'AlmaLinux' /etc/system-release ; then
+  # work out what we need to do here for AlmaLinux 
+  cd ~
+  
+else
+  cd ~
+  
+fi
+
+# end remove elasticpress config 
+######################################################################################################################
 
