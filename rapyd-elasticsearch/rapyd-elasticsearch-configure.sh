005c989243ba7db419b4e1127923b91c0f0636f8
Ayush <ayush@buddyboss.com>
2024-04-29 19:15:46 +0530

    restart elastic search when config changes.

--- rapyd-elasticsearch/rapyd-elasticsearch-configure.sh
+++ rapyd-elasticsearch/rapyd-elasticsearch-configure.sh
@@ -3,7 +3,8 @@
 # must run as root user.
 
 # Config file
-configPath=/etc/elasticsearch/jvm.options.d/rapyd-heap.options;
+configDir=/etc/elasticsearch/jvm.options.d/;
+configPath=${configDir}rapyd-heap.options;
 
 # Get available ram in node
 ramAvailable=$(($(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024 / 1024));
@@ -19,4 +20,5 @@ maxHeap=$(( $ramAvailable * $heapPercentage / 100));
 
 # create the configuration.
 rm -f $configPath;
+mkdir -p $configDir; # make sure dir exists.
 echo -e "-Xms${maxHeap}g\n-Xmx${maxHeap}g" > $configPath;
