3e28418a5f665fc9f6506d989444b70408da3370
Ayush <ayush@buddyboss.com>
2024-05-03 17:37:34 +0530

    unset the wp config constant.

--- rapyd-elasticsearch-plugin/clear.sh
+++ rapyd-elasticsearch-plugin/clear.sh
@@ -22,6 +22,7 @@ then
      then
        wp --skip-plugins --skip-themes --quiet plugin deactivate elasticpress   2>/dev/null || true
        wp --skip-plugins --skip-themes --quiet plugin uninstall elasticpress  2>/dev/null || true
+       wp --skip-plugins --skip-themes --quiet  config delete EP_HOST  2>/dev/null || true
     fi
 fi
 
