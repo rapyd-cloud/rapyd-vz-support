aec9a4d3762c851b39c2d2929f440b618a0bf0b2
Ayush <ayush@buddyboss.com>
2024-05-05 02:31:18 +0530

    Improve JPS and using heap size in m to be more precise

--- rapyd-elasticsearch/rapyd-elasticsearch-configure.sh
+++ rapyd-elasticsearch/rapyd-elasticsearch-configure.sh
@@ -3,22 +3,32 @@
 # must run as root user.
 
 # Config file
-configDir=/etc/elasticsearch/jvm.options.d/;
-configPath=${configDir}rapyd-heap.options;
+configDir=/etc/elasticsearch/jvm.options.d/
+configPath=${configDir}rapyd-heap.options
 
-# Get available ram in node
-ramAvailable=$(($(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024 / 1024));
+# Get available ram in node in MB
+ramAvailable=$(($(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024))
 
 # Get the max specifed heap to this node.
 if [[ $1 =~ ^[0-9]+$ ]]; then
     heapPercentage=$1
 else
-    heapPercentage=20
+    heapPercentage=70
 fi
 
-maxHeap=$(( $ramAvailable * $heapPercentage / 100));
+maxHeap=$(printf "%.0f" $(echo "$ramAvailable * $heapPercentage / 100" | bc))
+unit=m
+
+# if (($maxHeap < 1024)); then
+#     unit=m
+# else
+#     unit=g
+#     maxHeapGB=$(echo "scale=2; $maxHeap / 1024" | bc)
+#     maxHeap=$(printf "%.0f" $maxHeapGB)
+# fi
 
 # create the configuration.
-rm -f $configPath;
-mkdir -p $configDir; # make sure dir exists.
-echo -e "-Xms${maxHeap}g\n-Xmx${maxHeap}g" > $configPath;
+rm -f $configPath
+mkdir -p $configDir # make sure dir exists.
+echo -e "-Xms${maxHeap}${unit}\n-Xmx${maxHeap}${unit}" >$configPath
+cat $configPath
