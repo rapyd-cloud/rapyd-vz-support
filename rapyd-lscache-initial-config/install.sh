c2d2318d413d806fc70615e731708576b9efd458
Ayush <ayush@buddyboss.com>
2024-05-21 18:47:59 +0530

    Update litespeed addon.

--- rapyd-lscache-initial-config/install.sh
+++ rapyd-lscache-initial-config/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e;
+
 cd /var/www/webroot/ROOT/
 
 # get the current list of all active plugins 
@@ -9,35 +11,42 @@ SKIPLIST=$(wp plugin list --status=active --field=name --quiet --skip-plugins 2>
 
 wp plugin is-installed litespeed-cache --quiet --skip-plugins 2>/dev/null
 
+# ┌──────────────────────────────────────────────────────────────────────────┐
+# │ Make sure litespeed is installed & activate                              │
+# └──────────────────────────────────────────────────────────────────────────┘
+
 if [ "$?" -eq 0 ]
 then
-    wp plugin is-active litespeed-cache --quiet --skip-plugins 2>/dev/null
-    
-    if [ "$?" -eq 0 ]
-        then
-
-        ## disable the ls object cache before doing any other actions
-        wp litespeed-option set object false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null   
-    
-        ## forece update of lscache plugin 
-        wp plugin update litespeed-cache --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-    
-        wp litespeed-option set cache true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-
-        wp litespeed-option set cache-priv false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        wp litespeed-option set cache-commenter false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        wp litespeed-option set cache-rest false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        wp litespeed-option set cache-page_login false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        wp litespeed-option set cache-favicon true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        wp litespeed-option set cache-resources true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        wp litespeed-option set cache-mobile false  --quiet --skip-plugins="$SKIPLIST" 2>/dev/null     
-
-        wp litespeed-option set cache-exc $'^/wp-admin\n ^/wp-json \n^/wp-login\n^/register' --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        
-        wp litespeed-option set cache true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
-        
-    fi
+    wp plugin is-active litespeed-cache --quiet;
 else
-    echo "Installing LiteSpeed Plugin..."; 
-    wp plugin install litespeed-cache;
+    echo "Installing LiteSpeed Plugin...\n"; 
+    wp plugin install litespeed-cache --quiet;
 fi
+
+
+# ┌──────────────────────────────────────────────────────────────────────────┐
+# │ Update the litespeed WordPress plugin to the latest version.             │
+# └──────────────────────────────────────────────────────────────────────────┘
+
+
+echo "Updating Litespeed plugin...\n";
+wp plugin update litespeed-cache --quiet --skip-plugins="$SKIPLIST";
+
+Configure litespeed configuration for best rapyd usecase 
+
+
+# ┌──────────────────────────────────────────────────────────────────────────┐
+# │ Configure litespeed for Rapyd!                                           │
+# └──────────────────────────────────────────────────────────────────────────┘
+
+wp litespeed-option set object false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null  
+wp litespeed-option set cache true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache-priv false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache-commenter false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache-rest false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache-page_login false --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache-favicon true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache-resources true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache-mobile false  --quiet --skip-plugins="$SKIPLIST" 2>/dev/null     
+wp litespeed-option set cache-exc $'^/wp-admin\n ^/wp-json \n^/wp-login\n^/register' --quiet --skip-plugins="$SKIPLIST" 2>/dev/null
+wp litespeed-option set cache true --quiet --skip-plugins="$SKIPLIST" 2>/dev/null;
