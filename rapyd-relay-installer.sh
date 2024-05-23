4f8d02a70a5e7fa2e8bad7d5c996f65723a807b4
wesbuddyboss <106222721+wesbuddyboss@users.noreply.github.com>
2024-05-23 12:29:34 +1000

    Update rapyd-relay-installer.sh
    update relay to 0.8.0
    configure operating system specific build modules

--- rapyd-relay-installer.sh
+++ rapyd-relay-installer.sh
@@ -10,13 +10,27 @@ if [ -z "$RELAY_KEY" ]
   exit 9991
 fi
 
-RELAY_VERSION="v0.6.3"                         # https://builds.r2.relay.so/meta/latest
+# v0.8.0 release May 22 2024
+
+RELAY_VERSION="v0.8.0"                         # https://builds.r2.relay.so/meta/latest
 RELAY_PHP=$(php-config --version | cut -c -3)  # 8.1
 RELAY_INI_DIR=$(php-config --ini-dir)          # /etc/php/8.1/cli/conf.d/
 RELAY_EXT_DIR=$(php-config --extension-dir)    # /usr/lib/php/20210902
 RELAY_ARCH=$(arch | sed -e 's/arm64/aarch64/;s/amd64\|x86_64/x86-64/')
 
-RELAY_ARTIFACT="https://builds.r2.relay.so/$RELAY_VERSION/relay-$RELAY_VERSION-php$RELAY_PHP-debian-$RELAY_ARCH.tar.gz"
+# old debian build format
+# RELAY_ARTIFACT="https://builds.r2.relay.so/$RELAY_VERSION/relay-$RELAY_VERSION-php$RELAY_PHP-debian-$RELAY_ARCH.tar.gz"
+
+if grep -a 'AlmaLinux' /etc/system-release ; then
+  # centos and alma have different build versions
+  RELAY_ARTIFACT="https://builds.r2.relay.so/$RELAY_VERSION/relay-$RELAY_VERSION-php$RELAY_PHP-el9-$RELAY_ARCH.tar.gz"
+  
+else
+  # centos and alma have different build versions
+  RELAY_ARTIFACT="https://builds.r2.relay.so/$RELAY_VERSION/relay-$RELAY_VERSION-php$RELAY_PHP-centos7-$RELAY_ARCH.tar.gz"
+
+fi
+
 RELAY_TMP_DIR=$(mktemp -dt relay.XXXXXXXX)
 
 ## Download artifact
@@ -38,4 +52,3 @@ sed -i "s/^;\? \?relay.key =.*/relay.key = $RELAY_KEY/" $RELAY_TMP_DIR/relay.ini
 ## Move `relay.ini`
 rm -f $RELAY_INI_DIR/relay.ini
 yes | cp -rf $RELAY_TMP_DIR/relay.ini $RELAY_INI_DIR
-
