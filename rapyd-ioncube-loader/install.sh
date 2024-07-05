4d975a4cbbd88845c87d9125b9e6fc5f188978dd
Ayush <ayush@buddyboss.com>
2024-07-05 16:05:02 +0530

    IonCube Loader Addon.

---
+++ rapyd-ioncube-loader/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd /usr/local/rapyd/rapyd-ioncube-loader-files;
+
+# Download IonCube Libraries.
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.zip;
+unzip ioncube_loaders_lin_x86-64.zip;
+
+PHP_VERSION=$( php -i | head -n 5 | grep "PHP Version =>" |  grep -o '[0-9.]*' | grep -o '^[0-9]\+\.[0-9]\+');
+PHP_EXTENTION="/home/ioncubeloader/ioncube/ioncube_loader_lin_$PHP_VERSION.so";
+PHP_INI_DIR=$(php-config --ini-dir);      
+PHP_EXT_DIR=$(php-config --extension-dir);
+
+#================================================================
+# Check if this version of PHP is supported by IonCube Loader.
+#================================================================
+if [ -e "$PHP_EXTENTION" ]; then
+  
+  IONCUBE_LOADER_INI=$PHP_INI_DIR/ioncube-loader.ini;
+
+  # Install the extention file.
+  rm -f $RELAY_EXT_DIR/relay.so
+  yes | cp -rf $PHP_EXTENTION $RELAY_EXT_DIR/ioncube-loader.so
+
+  # Install ini file.
+  rm -f $IONCUBE_LOADER_INI;
+  echo "zend_extension = ioncube-loader.so" > $IONCUBE_LOADER_INI;
+
+  echo "ionCube Loader Installed!";  
+
+else
+  echo "PHP version is not supported by IonCube Loader."
+  exit 1;
+fi
