379a10800c9e29c16e5e8bbad7e61e7c4c55dffe
Ayush <ayush@rapyd.cloud>
2025-02-17 12:53:47 +0530

    Monarx 5.1 Addon

---
+++ rapyd-monarx-addon/rapyd-monarx-cleaner.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# must be run a sudo su - / root 
+
+# define logic variables to determine which monarx linked so file was deployed
+
+MONARX_PHP=$(php-config --version | cut -c -3) 
+MONARX_PHPSTRIPPED=${MONARX_PHP/.} 
+MONARX_INI_DIR=$(php-config --ini-dir)  
+MONARX_EXT_DIR=$(php-config --extension-dir)  
+MONARX_ARCH=$(arch | sed -e 's/arm64/aarch64/;s/amd64\|x86_64/x86-64/') 
+
+MONARX_SOPATH="/usr/lib64/monarx-protect"
+MONARX_SO="monarxprotect-php$MONARX_PHPSTRIPPED.so"
+MONARX_INI_FILEPATH="$MONARX_INI_DIR/monarxprotect.ini"
+MONARX_LINKED="$MONARX_EXT_DIR/$MONARX_SO"
+
+# clean up any existing linked file 
+if [ -L "$MONARX_LINKED" ]; then
+  rm -f "$MONARX_LINKED"
+fi
+
+# clean up and existing monarxprotect.ini file 
+if [ -f "$MONARX_INI_FILEPATH" ]; then
+  rm -f "$MONARX_INI_FILEPATH"
+fi
+
+# remove conf file
+rm -f  /etc/monarx-agent.conf
+
+# remove monarx if possible
+yum remove monarx-protect-autodetect -y
+yum remove monarx-protect -y
+yum remove monarx-agent -y
+
+mkdir -p /usr/local/rapyd/
+mkdir -p /usr/local/rapyd/rapyd-monarx-files
+chown litespeed:litespeed /usr/local/rapyd
+chown litespeed:litespeed /usr/local/rapyd/rapyd-monarx-files
+
+cd /usr/local/rapyd/rapyd-monarx-files
+
+rm -f rapyd-monarx-installer.sh
+rm -f rapyd-monarx-linker.sh
+rm -f rapyd-monarx-cleaner.sh
+
+#end of monarx cleanup logic
---
+++ rapyd-monarx-addon/rapyd-monarx-installer.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+
+source /etc/profile
+
+# must be run as sudo su - 
+# must pass in vzenvironmentname , vzuid , vznodeid
+
+#load parameters
+VZENVNAME=$1
+VZUID=$2
+VZNODEID=$3
+
+if [ -z "$VZENVNAME" ]
+  then
+  exit 9991
+fi
+if [ -z "$VZUID" ]
+  then
+  exit 9992
+fi
+
+if [ -z "$VZNODEID" ]
+  then
+  exit 99993
+fi
+
+# setup defaults
+IS_TESTING=false
+IS_STAGING=false
+IS_DEVELOPER=false
+
+# remove any old monarx-agent configuration file
+cd /etc/
+rm -f monarx-agent.conf
+
+# create monarx-agent configuration file 
+echo "#########################################################################" > monarx-agent.conf
+echo "# Rapyd Monarx Customer Deployment" >> monarx-agent.conf
+echo "#########################################################################" > monarx-agent.conf
+
+echo "client_id=id_live_vAcPku6RwAUoemSNFKRbZMX2" >> monarx-agent.conf
+echo "client_secret=sk_live_sb4s7Wvvvh2DIx4L6gH5KHwS" >> monarx-agent.conf
+
+echo "host_id=$VZNODEID-$VZENVNAME" >> monarx-agent.conf
+
+echo "exclude_dirs=/virtfs" >> monarx-agent.conf
+echo "exclude_dirs=/(clam_|\.)?quarantine" >> monarx-agent.conf
+echo "exclude_users=^virtfs$" >> monarx-agent.conf
+
+echo "user_base=/var/www/webroot/ROOT/" >> monarx-agent.conf
+echo "user_base=/usr/local/lsws/" >> monarx-agent.conf
+echo "user_base=/tmp/" >> monarx-agent.conf
+echo "user_base=/home/litespeed/" >> monarx-agent.conf
+
+echo "tags=$HOSTNAME" >> monarx-agent.conf
+echo "tags=$VZNODEID" >> monarx-agent.conf
+echo "tags=$VZENVNAME" >> monarx-agent.conf
+echo "tags=UID:$VZUID" >> monarx-agent.conf
+echo "tags=NODEID:$VZNODEID" >> monarx-agent.conf
+echo "tags=ENVNAME:$VZENVNAME" >> monarx-agent.conf
+
+if [[ "$RAPYD_PLAN" == *"STAGING"* ]]
+  then
+    IS_STAGING=true
+fi
+
+if [[ "$VZENVNAME" == *"-staging"* ]] 
+  then
+    IS_STAGING=true
+fi
+
+if [[ "$VZENVNAME" == "stg-"* ]]
+  then
+    IS_STAGING=true
+fi
+
+if [[ "$HOSTNAME" == *"rapydapps.cloud"* ]]
+  then
+    echo "tags=rapydapps.cloud" >> monarx-agent.conf  
+fi
+
+if [[ "$HOSTNAME" == *"rapyd.cloud"* ]]
+  then
+    echo "tags=rapyd.cloud" >> monarx-agent.conf  
+    IS_STAGING=true
+    IS_TESTING=true
+fi
+
+if [[ "$HOSTNAME" == *"developbb.dev"* ]] 
+  then
+    echo "tags=developbb.dev" >> monarx-agent.conf  
+    IS_STAGING=true
+    IS_DEVELOPER=true
+fi
+
+totalk=$(awk '/MemTotal:/{print $2}' /proc/meminfo)
+devmemlimit=2100000 
+if [[ $totalk -lt $devmemlimit ]]
+  then
+    IS_DEVELOPER=true
+fi
+
+if [[ "$RAPYD_PLAN" == *"DEVELOPER"* ]]
+  then
+    IS_DEVELOPER=true
+fi
+
+if [[ "$RAPYD_PLAN" == "DEV"* ]]
+  then
+    IS_DEVELOPER=true
+fi
+
+if [[ "$IS_TESTING" = true ]]
+  then
+    echo "tags=test" >> monarx-agent.conf
+    echo "tags=testing" >> monarx-agent.conf    
+fi
+
+if [[ "$IS_STAGING" = true ]]
+  then
+    echo "tags=staging" >> monarx-agent.conf
+fi
+
+if [[ "$IS_DEVELOPER" = true ]] 
+  then
+    echo "tags=dev" >> monarx-agent.conf
+    echo "tags=developer" >> monarx-agent.conf
+fi
+
+echo "tags=litespeed" >> monarx-agent.conf
+echo "tags=vz" >> monarx-agent.conf
+
+echo "#########################################################################" >> monarx-agent.conf
+
+now=$(date)
+echo "# deployed: $now"  >> monarx-agent.conf
+
+echo "#########################################################################" >> monarx-agent.conf
+
+
+
+if grep -a 'AlmaLinux' /etc/system-release ; then
+  # work out what we need to do here for AlmaLinux 
+  cd ~
+  
+  # force stop monarx  if it happens to be running 
+  sudo systemctl stop monarx-agent
+
+  # install the repository repo and pgp key
+  cd /tmp
+  sudo curl -o /etc/yum.repos.d/monarx.repo https://repository.monarx.com/repository/monarx-yum/linux/yum/el/9/x86_64/monarx.repo
+  sudo rpm --import https://repository.monarx.com/repository/monarx/publickey/monarxpub.gpg
+  
+  # install monarx
+  sudo yum install monarx-protect-autodetect -y
+
+  # force stop monarx  if it happens to be running 
+  sudo systemctl stop monarx-agent
+
+  # force update monarx
+  sudo yum update monarx-agent -y
+
+  # force restart 
+  sudo systemctl restart monarx-agent
+  
+else
+  # assume this is the current Centos 7 based platform install
+  cd ~
+
+  # force stop monarx  if it happens to be running 
+  sudo systemctl stop monarx-agent
+
+  # install the repository repo and pgp key
+  cd /tmp
+  sudo curl -o /etc/yum.repos.d/monarx.repo https://repository.monarx.com/repository/monarx-yum/linux/yum/el/7/x86_64/monarx.repo
+  sudo rpm --import https://repository.monarx.com/repository/monarx/publickey/monarxpub.gpg
+
+  # install monarx
+  sudo yum install monarx-protect-autodetect -y
+
+  # force stop monarx  if it happens to be running 
+  sudo systemctl stop monarx-agent
+
+  # force update monarx
+  sudo yum update monarx-agent -y
+
+  # force restart 
+  sudo systemctl restart monarx-agent
+
+fi
+
+
+# end of monarx main deployer
+
+
+
+
+
+
+
+
