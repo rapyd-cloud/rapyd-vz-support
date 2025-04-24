a5f88c71e86b580e6d30e7880eaa2c35eda9dafc
Max Skupin <max@rapyd.cloud>
2025-04-24 12:37:04 -0500

    Add files via upload
    Relocating old installer files

---
+++ rapyd-ffmpeg-cleaner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# must be run as root - su user
+
+######################################################################################################################
+# ffmpeg 
+
+if grep -a 'AlmaLinux' /etc/system-release ; then
+  # work out what we need to do here for AlmaLinux 
+  cd ~
+  echo "Removing ffmpeg for AlmaLinux"
+
+  sudo dnf remove ffmpeg ffmpeg-devel -y
+  
+else
+  cd ~
+  echo "Removing ffmpeg for Centos"
+
+  sudo yum remove ffmpeg ffmpeg-devel -y
+
+fi
