43f3d6376be77a4e5ae21790849c87386fa8e198
Ayush <ayush@rapyd.cloud>
2025-09-06 02:53:31 +0530

    remove not related change.

--- rapyd-relay-addon/rapyd-relay-installer.sh
+++ rapyd-relay-addon/rapyd-relay-installer.sh
@@ -11,10 +11,10 @@ if [ -z "$RELAY_KEY" ]
 fi
 
 # Updated to v0.11.0
-RELAY_VERSION="v0.11.0"                         # https://builds.r2.relay.so/meta/latest
-RELAY_PHP=$(php-config --version | cut -c -3)   # 8.1
-RELAY_INI_DIR=$(php-config --ini-dir)           # /etc/php/8.1/cli/conf.d/
-RELAY_EXT_DIR=$(php-config --extension-dir)     # /usr/lib/php/20210902
+RELAY_VERSION="v0.11.0"                        # https://builds.r2.relay.so/meta/latest
+RELAY_PHP=$(php-config --version | cut -c -3)  # 8.1
+RELAY_INI_DIR=$(php-config --ini-dir)          # /etc/php/8.1/cli/conf.d/
+RELAY_EXT_DIR=$(php-config --extension-dir)    # /usr/lib/php/20210902
 RELAY_ARCH=$(arch | sed -e 's/arm64/aarch64/;s/amd64\|x86_64/x86-64/')
 
 # old debian build format
@@ -51,8 +51,5 @@ sed -i "s/^;\? \?relay.key =.*/relay.key = $RELAY_KEY/" $RELAY_TMP_DIR/relay.ini
 ## Move `relay.ini`
 rm -f $RELAY_INI_DIR/relay.ini
 yes | cp -rf $RELAY_TMP_DIR/relay.ini $RELAY_INI_DIR
-
-## Restart LiteSpeed
-service lsws restart
 
 ## relay deployed
