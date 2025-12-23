94141795943378906c4d4a5325afbdaaf255d2e8
Ayush <ayush@rapyd.cloud>
2025-12-23 18:22:24 +0530

    typo fix.

--- helper/rsync-llsmp-2-lomp.sh
+++ helper/rsync-llsmp-2-lomp.sh
@@ -3,27 +3,27 @@ set -Eeuo pipefail
 
 trap 'echo "❌ Error at line $LINENO. Exiting."; exit 1' ERR
 
-echo "Copying /mnt/llsmp-home to /home/";
+echo "Copying /mnt/llsmp-home/ to /home/";
 rsync -a --delete \
   --exclude='litespeed/' \
   --exclude='jelastic/' \
   /mnt/llsmp-home/ /home/
 
-echo "Copying /mnt/llsmp-acme.sh to /root/.acme.sh";
+echo "Copying /mnt/llsmp-acme.sh/ to /root/.acme.sh";
 rsync -a --delete \
   /mnt/llsmp-acme.sh/ /root/.acme.sh/
 
-echo "Copying /mnt/llsmp-rapyd-rsm to /etc/rapyd-rsm";
+echo "Copying /mnt/llsmp-rapyd-rsm/ to /etc/rapyd-rsm";
 rsync -a --delete \
-  /mnt/llsmp-rapyd-rsm /etc/rapyd-rsm
+  /mnt/llsmp-rapyd-rsm/ /etc/rapyd-rsm
 
-echo "Copying /mnt/llsmp-mysql to /var/lib/mysql";
+echo "Copying /mnt/llsmp-mysql/ to /var/lib/mysql";
 rsync -a --delete \
-  /mnt/llsmp-mysql /var/lib/mysql
+  /mnt/llsmp-mysql/ /var/lib/mysql
 
 echo "Copying /mnt/llsmp-cron to /var/spool/cron";
 rsync -a --delete \
-  /mnt/llsmp-cron /var/spool/cron
+  /mnt/llsmp-cron/ /var/spool/cron
 
 echo "Copying /mnt/llsmp-www/conf/httpd_config.xml to /var/www/conf/httpd_config.xml";
 rsync -a \
