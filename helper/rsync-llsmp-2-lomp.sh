703db272b39c7b30c5aa09bc23dc39e59c9c05c1
Ayush <ayush@rapyd.cloud>
2025-12-23 14:51:43 +0530

    update rsync llsmp 2 lomp bash.

---
+++ helper/rsync-llsmp-2-lomp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+trap 'echo "❌ Error at line $LINENO. Exiting."; exit 1' ERR
+
+echo "Copying /mnt/home/ to /home/";
+rsync -a --delete \
+  --exclude='litespeed/' \
+  --exclude='jelastic/' \
+  /mnt/home/ /home/
+
+echo "Copying /mnt/llsmp-acme.sh to /root/.acme.sh";
+rsync -a --delete \
+  /mnt/llsmp-acme.sh/ /root/.acme.sh/
+
+echo "Copying /mnt/llsmp-rapyd-rsm to /etc/rapyd-rsm";
+rsync -a --delete \
+  /mnt/llsmp-rapyd-rsm /etc/rapyd-rsm
+
+echo "Copying /mnt/llsmp-mysql to /var/lib/mysql";
+rsync -a --delete \
+  /mnt/llsmp-mysql /var/lib/mysql
+
+echo "Copying /mnt/llsmp-cron to /var/spool/cron";
+rsync -a --delete \
+  /mnt/llsmp-cron /var/spool/cron
+
+echo "Copying /mnt/llsmp-www/conf/httpd_config.xml to /var/www/conf/httpd_config.xml";
+rsync -a \
+/mnt/llsmp-www/conf/httpd_config.xml /var/www/conf/httpd_config.xml
+
+echo "{{SUCCESS}}";
